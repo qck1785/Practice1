0062f10e5f79ed40a4e30d839f4859a97fe38cc2
Aleksander <Aleksander@DESKTOP-MQ43BQ7>
2025-04-15 15:10:34 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/OneToFive.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp1
+{
+    internal class OneToFive
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Какой-то заголовок";
+            /*Console.WriteLine(" C# — это современный, с открытым исходным кодом, объектно-ориентированный язык программирования.");
+            Console.WriteLine("Нажмите на любую клавишу");
+            Console.ReadKey();*/
+
+            /*Console.WriteLine("C# — это современный,\n\t с открытым исходным кодом,\n\t\t объектно-ориентированный\n\t\t\t язык программирования.");
+            Console.WriteLine("Нажмите на любую клавишу");
+            Console.ReadKey();*/
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("C# — это современный, с открытым исходным кодом, объектно-ориентированный язык программирования.");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Нажмите на любую клавишу");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/SixToTwelve.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp2
+{
+    internal class SixToTwelve
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Работа 1 Проект 2."; // задать название окна
+            Console.WindowWidth = 60; // задать ширину окна
+            Console.WindowHeight = 13; //  задать длину окна
+            Console.BackgroundColor = ConsoleColor.Blue; // задать цвет фона у окна
+            Console.Clear(); //очистить консоль
+            Console.SetCursorPosition(15, 6); // установить курсор на позицию
+
+            //Console.Write("Введите своё имя: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Здравствуйте, " + name + "!");
+            //Console.Write("Для заврешения нажмите на любую клавишу... ");   
+            //Console.ReadKey();
+
+            Console.Write("Введите своё имя: ");
+            string name = Console.ReadLine();
+            Console.Write("Введите свою фамилию: ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, {1} {0}!", surname, name);
+            Console.Write("Для заврешения нажмите на любую клавишу... ");
+            Console.ReadKey();
+
+        }
+    }
+}
